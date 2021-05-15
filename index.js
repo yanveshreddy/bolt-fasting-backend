992fa76d29666492d8a4be2cd93977585e438718
yanveshreddy <anvesh.learnings@gmail.com>
2021-05-15 20:38:14 +0530

    initial setup commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const path = require("path");
+
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const app = express();
+
+const dbPath = path.join(__dirname, "boltfasting.db");
+
+let db = null;
+
+app.get("/", (req, res) => {
+  res.send("welcome anvesh");
+});
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3001, () => {
+      console.log("Server Running at http://localhost:3001/");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDBAndServer();
